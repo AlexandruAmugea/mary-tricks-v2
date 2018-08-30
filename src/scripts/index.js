ce4bd715515bcdddc71ca66a2b1a6062b5192942
Alexandru.Amugea <alexandruamugea@gmail.com>
2018-08-30 17:11:24 +0300

    Added carousel and services

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -52,3 +52,18 @@ function config($routeProvider) {
 
 app.config(config);
 
+// Add bootstrap carousel number of slides
+
+let totalItems = $('.item').length;
+let currentIndex = $('div.active').index() + 1;
+$('.slides-number').html(`${currentIndex} / ${totalItems}`);
+// Bind number of slides
+
+$('#myCarousel').on('slide.bs.carousel', function () {
+    //Do something
+});
+$('#myCarousel').on('slid.bs.carousel', function () {
+    currentIndex = $('div.active').index() + 1;
+    $('.slides-number').html(`${currentIndex} / ${totalItems}`);
+});
+
